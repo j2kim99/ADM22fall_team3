57a09c5e03a3983d8cf097f426729a8ec0927a8f
Jaemin Park <50475885+jminnnnn@users.noreply.github.com>
2022-12-05 15:36:23 +0900

    fastpi added

---
+++ fastpi.py
@@ -0,0 +1,70 @@
+from scipy.sparse.linalg import svds
+from scipy.sparse import csc_matrix, csr_matrix, lil_matrix, bmat, eye
+from scipy.sparse import random, block_diag, diags
+from networkx.algorithms import bipartite
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from heapq import nlargest
+plt.rcParams['figure.figsize'] = [12, 8]
+plt.rcParams['figure.dpi'] = 100
+
+
+def to_graph(A):
+    return bipartite.matrix.from_biadjacency_matrix(A)
+
+def gcc(graph):
+    return graph.subgraph(max(nx.connected_components(graph), key=len))
+
+def top_bottom(graph):
+    top_nodes = {n for n, d in graph.nodes(data=True) if d["bipartite"] == 0}
+    bottom_nodes = set(graph) - top_nodes
+    return top_nodes, bottom_nodes
+
+def largest_hubs(graph, k=0.2):
+    top, bottom = top_bottom(graph)
+    m_top = max(1, int(len(top) * k))
+    m_bot = max(1, int(len(bottom) * k))
+    degree = lambda x: graph.degree[x]
+    top_largest = nlargest(m_top, top, key=degree)
+    bottom_largest = nlargest(m_bot, bottom, key=degree)
+    return top_largest, bottom_largest
+
+def reduce(graph, k=0.2):
+    top_largest, bottom_largest = largest_hubs(graph, k)
+    graph.remove_nodes_from(top_largest + bottom_largest)
+    new_graph = gcc(graph).copy()
+    graph.remove_nodes_from(new_graph.nodes())
+    print(len(graph), len(new_graph), len(top_largest + bottom_largest))
+    return (*top_bottom(graph), new_graph, top_largest, bottom_largest)
+
+def fastpi(A, k=0.2):
+    graph = to_graph(A)
+    top, bottom = top_bottom(graph)
+    m_top = max(1, int(len(top) * k))
+    m_bot = max(1, int(len(bottom) * k))
+    nt = []
+    nb = []
+    perm_top_lhs = []
+    perm_top_rhs = []
+    perm_bot_lhs = []
+    perm_bot_rhs = []
+    
+    while m_top < len(top) and m_bot < len(bottom):
+        m_top = max(1, int(len(top) * k))
+        m_bot = max(1, int(len(bottom) * k))
+        ptl, pbl, graph, ptr, pbr = reduce(graph, k)
+        perm_top_lhs = perm_top_lhs + list(ptl)
+        perm_bot_lhs = perm_bot_lhs + list(pbl)
+        perm_top_rhs = list(ptr) + perm_top_rhs
+        perm_bot_rhs = list(pbr) + perm_bot_rhs
+        nt.append(len(perm_top_lhs))
+        nb.append(len(perm_bot_lhs))
+        top, bottom = top_bottom(graph)
+        
+    perm_top = np.array(perm_top_lhs + list(top) + perm_top_rhs)
+    perm_bot = np.array(perm_bot_lhs + list(bottom) + perm_bot_rhs) - A.shape[0]
+    
+    
+    return perm_top
+
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
 import click
-from time import time
 from util import *
 
+from fastpi import *
 from simple import *
 from IBSO import *
 from slashburn import *
 @click.command()
 @click.option('--data', type=str, default='minibipartite')
-@click.option('--method', type=str, default='random')
+@click.option('--method', type=str, default='degsort')
 @click.option('--size', type=int, default=2)
 @click.option('--dbg', type=bool, default=False)
 @click.option('--showmat', type=bool, default=False)
@@ -21,19 +21,12 @@ def main(data, method, size, dbg, showmat, seed):
     G, P1, P2 = arrange_bipartite(G, P1, P2)
     if showmat:
         print_adjmat(G, list(range(V)), size)
-
     if dbg:
         print(f'V:{V}, P1:{len(P1)}, P2:{len(P2)}, E:{E}')
-    #print(G)
     set_seed(seed)
-    start_time = time()
-    if method.casefold() == 'random'.casefold() or method.casefold() == 'rand'.casefold():
-        perm = random_perm(G)
-    elif method.casefold() == 'bipartite_random'.casefold() or method.casefold() == 'birand'.casefold():
-        perm = bipartite_random(G, P1, P2)
-    elif method.casefold() == 'degsort'.casefold() or method.casefold() == 'deg'.casefold():
+    if method.casefold() == 'degsort'.casefold() or method.casefold() == 'deg'.casefold():
         perm = degsort(G)
-    elif method.casefold() == 'bideg'.casefold():
+    if method.casefold() == 'bideg'.casefold():
         perm = bipartite_degsort(G, P1, P2)
     elif method.casefold() == 'BFS'.casefold():
         perm = BFS(G)
@@ -44,14 +37,20 @@ def main(data, method, size, dbg, showmat, seed):
     elif method.casefold() =='SB'.casefold() or method.casefold()=='slashburn'.casefold():
         cooG=preprocess(V,G)
         perm =slashburn(cooG)
+
+        
     elif method.casefold() == 'IBSO'.casefold():
         perm = IBSO(G, P1, P2, size)
-    elapsed_time = time()-start_time
+        
+    elif method.casefold() =='fp'.casefold() or method.casefold()=='fastpi'.casefold():
+        cooG=preprocess(V,G)
+        perm = fastpi(cooG)
+
     if dbg:
         print('lenperm:', len(perm))
     blks = count_blocks(G, perm, size)
     print("nonzero blocks:", blks)
-    print(f"elapsed time: {elapsed_time:.4f} sec")
+    
     if showmat:
         print_adjmat(G, perm, size)
 

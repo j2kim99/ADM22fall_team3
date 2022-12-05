7589e484903c5b46d13950e4fb80076db5df3e13
j2kim99 <j2kim99@snu.ac.kr>
2022-12-05 16:45:43 +0900

    fastpi

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import click
+from time import time
 from util import *
 
-from fastpi import *
 from simple import *
 from IBSO import *
 from slashburn import *
+from fastpi import *
 @click.command()
 @click.option('--data', type=str, default='minibipartite')
-@click.option('--method', type=str, default='degsort')
+@click.option('--method', type=str, default='random')
 @click.option('--size', type=int, default=2)
 @click.option('--dbg', type=bool, default=False)
 @click.option('--showmat', type=bool, default=False)
@@ -21,12 +22,19 @@ def main(data, method, size, dbg, showmat, seed):
     G, P1, P2 = arrange_bipartite(G, P1, P2)
     if showmat:
         print_adjmat(G, list(range(V)), size)
+
     if dbg:
         print(f'V:{V}, P1:{len(P1)}, P2:{len(P2)}, E:{E}')
+    #print(G)
     set_seed(seed)
-    if method.casefold() == 'degsort'.casefold() or method.casefold() == 'deg'.casefold():
+    start_time = time()
+    if method.casefold() == 'random'.casefold() or method.casefold() == 'rand'.casefold():
+        perm = random_perm(G)
+    elif method.casefold() == 'bipartite_random'.casefold() or method.casefold() == 'birand'.casefold():
+        perm = bipartite_random(G, P1, P2)
+    elif method.casefold() == 'degsort'.casefold() or method.casefold() == 'deg'.casefold():
         perm = degsort(G)
-    if method.casefold() == 'bideg'.casefold():
+    elif method.casefold() == 'bideg'.casefold():
         perm = bipartite_degsort(G, P1, P2)
     elif method.casefold() == 'BFS'.casefold():
         perm = BFS(G)
@@ -37,17 +45,17 @@ def main(data, method, size, dbg, showmat, seed):
     elif method.casefold() =='SB'.casefold() or method.casefold()=='slashburn'.casefold():
         cooG=preprocess(V,G)
         perm =slashburn(cooG)
-    elif method.casefold() == 'IBSO'.casefold():
-        perm = IBSO(G, P1, P2, size)  
     elif method.casefold() =='fp'.casefold() or method.casefold()=='fastpi'.casefold():
         cooG=preprocess(V,G)
         perm = fastpi(cooG)
-
+    elif method.casefold() == 'IBSO'.casefold():
+        perm = IBSO(G, P1, P2, size)
+    elapsed_time = time()-start_time
     if dbg:
         print('lenperm:', len(perm))
     blks = count_blocks(G, perm, size)
     print("nonzero blocks:", blks)
-    
+    print(f"elapsed time: {elapsed_time:.4f} sec")
     if showmat:
         print_adjmat(G, perm, size)
 
